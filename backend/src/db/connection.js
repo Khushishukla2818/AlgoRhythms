13e463dc2c7fdd2c0adf8ba8faa1e6a0ef6694e6
khushi shukla <149226589+Khushishukla2818@users.noreply.github.com>
2025-09-14 01:37:56 +0530

    Update connection.js

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        // Use env variable directly
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
         });
         console.log(`MongoDB connected successfully: ${connectionInstance.connection.host}`);
     } catch (error) {
