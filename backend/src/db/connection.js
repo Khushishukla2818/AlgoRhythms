d3f23361d5546860ee736ad4ccde044b9bee6628
khushi shukla <149226589+Khushishukla2818@users.noreply.github.com>
2025-09-14 01:28:57 +0530

    MongodbURL

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log(`MongoDB connected successfully: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("Error connecting to MongoDB", error);
