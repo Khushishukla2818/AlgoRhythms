c7d31ee5cb5732f8fcba8532ed63c4ac705a7c9c
khushi shukla <149226589+Khushishukla2818@users.noreply.github.com>
2025-09-14 01:37:08 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,14 @@ import dotenv from 'dotenv';
 import connectDB from './db/connection.js';
 import app from './app.js';
 
+dotenv.config(); 
 
-dotenv.config();
-
-// Connect to MongoDB
-connectDB()
-    .then(() => {
-        const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB', error);
-        process.exit(1);
+connectDB().then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
     });
+}).catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+    process.exit(1);
+});
